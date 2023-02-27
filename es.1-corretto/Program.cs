167ff21209c05921958986a8e8ae07c25a77f7ab
cahinosilva.20184 <cahinosilva.20184@LABI2-13PC05>
2023-02-27 13:27:50 +0100

    Aggiungere i file di progetto.

---
+++ es.1-corretto/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es._1_corretto
+{
+    internal class Program
+    {
+        static void compara(int x, int y)
+        {
+            if(x<y)
+            {
+                Console.WriteLine("L'ordine esatto è: "+ x+y);
+            }
+            else if(x>y)
+            {
+                Console.WriteLine("L'ordine esatto è:"+y+x);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inseire primo numero: ");
+            int x=int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserire secondo numero");
+            int y=int.Parse(Console.ReadLine());
+        }
+    }
+}
